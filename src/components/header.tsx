defebc3f1e91e106f7224df4e850c1edbaf3b88e
John Fash <john@Johns-MacBook-Pro.local>
2023-09-11 10:09:44 +0100

    fixed header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,18 +2,18 @@ import Link from "next/link";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faLinkedin, faWhatsapp, faGithub, faInstagram } from '@fortawesome/free-brands-svg-icons'
 const Header = () => {
-  return <div className="bg-white z-10 border-b top-0 left-0 right-0 fixed flex w-full px-20 py-3 justify-between items-center">
+  return <div className="bg-white flex-wrap z-10 border-b top-0 left-0 right-0 fixed flex w-full px-20 py-3 justify-between items-center">
     <div>
       <h1 className="text-[1.4rem] text-shadow-sm font-[800] tracking-widest py-2">JOHNFASH</h1>
     </div>
     <div>
-      <Link href='/' className="px-6 py-3">Home</Link>
-      <Link href='#projects' className="px-6 py-3">Projects</Link>
-      <Link href='#about' className="px-6 py-3">About</Link>
-      <Link href='#team' className="px-6 py-3">Team</Link>
+      <Link href='/' className="pr-3 md:pr-6 sm:pl-3 md:pl-6 py-3">Home</Link>
+      <Link href='#projects' className="px-3 md:px-6 py-3">Projects</Link>
+      <Link href='#about' className="px-3 md:px-6 py-3">About</Link>
+      <Link href='#team' className="px-3 md:px-6 py-3">Team</Link>
     </div>
-    <div>
-      <Link href='https://linkedin.com/in/johnfash' className="p-2 text-xl">
+    <div className="mt-3 sm:mt-0">
+      <Link href='https://linkedin.com/in/johnfash' className="pl-0 sm:pl-2 p-2 text-xl">
         <FontAwesomeIcon icon={faLinkedin} />
       </Link>
       <Link href='https://github.com/johnsonfash' className="p-2 text-xl">
